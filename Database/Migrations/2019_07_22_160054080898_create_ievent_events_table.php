cfb30931b71801ef42a1636d56e1aadbbb124472
Yeison Tapia <yftapial@ut.edu.co>
2019-08-16 17:09:25 -0500

    + Update migration events

--- Database/Migrations/2019_07_22_160054080898_create_ievent_events_table.php
+++ Database/Migrations/2019_07_22_160054080898_create_ievent_events_table.php
@@ -15,8 +15,8 @@ class CreateIeventEventsTable extends Migration
     Schema::create('ievent__events', function (Blueprint $table) {
       $table->engine = 'InnoDB';
       $table->increments('id');
-      $table->date('start_date');
-      $table->date('end_date');
+      $table->timestamp('start_date');
+      $table->timestamp('end_date');
       $table->boolean('repeat');
       $table->boolean('all_day');
       $table->text('address');
@@ -25,7 +25,7 @@ class CreateIeventEventsTable extends Migration
       $table->double('price', 8, 2);
       $table->integer('organizer_id')->unsigned()->nullable();
       $table->foreign('organizer_id')->references('id')->on('ievent__organizers')->onDelete('restrict');
-      $table->boolean('status');
+      $table->integer('status')->default(0)->unsigned();
       $table->text('options');
       $table->integer('user_id')->unsigned()->nullable();
       $table->foreign('user_id')->references('id')->on(config('auth.table', 'users'))->onDelete('restrict');
