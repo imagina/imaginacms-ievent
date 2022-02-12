d0f842603842aa0c9a0808b649af9cd82a80bbe8
Michael <msolano@imaginacolombia.com>
2022-02-11 19:09:02 -0500

    +Update: cms Translation files

--- Resources/lang/en/cms.php
+++ Resources/lang/en/cms.php
@@ -1,7 +1,14 @@
 <?php
+
 return [
-  "newEvent"  => 'New Event',
-  "updateEvent"  => 'Update Event',
-  "newCategory"  => 'New Category',
-  "updateCategory"  => 'Update Category'
+  "sidebar" => [
+    "adminGroup" => "Events",
+    "adminEvents" => "My Events",
+    "adminCategories" => "Event Categories"
+  ],
+  "newEvent" => "New Event",
+  "updateEvent" => "Update Event",
+  "newCategory" => "New Category",
+  "updateCategory" => "Update Category"
 ];
+
--- Resources/lang/es/cms.php
+++ Resources/lang/es/cms.php
@@ -1,7 +1,14 @@
 <?php
+
 return [
-  "newEvent"  => 'Nuevo Evento',
-  "updateEvent"  => 'Actualizar Evento',
-  "newCategory"  => 'Nueva Categoría',
-  "updateCategory"  => 'Actualizar Categoría'
+  "sidebar" => [
+    "adminGroup" => "Eventos",
+    "adminEvents" => "Mis Eventos",
+    "adminCategories" => "Categorias de Eventos"
+  ],
+  "newEvent" => "Nuevo Evento",
+  "updateEvent" => "Actualizar Evento",
+  "newCategory" => "Nueva Categoría",
+  "updateCategory" => "Actualizar Categoría"
 ];
+
