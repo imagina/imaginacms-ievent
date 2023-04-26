3256b8baedec81fb4a295d430ab4d3d93aa79736
Cristian Marin <marin101998@gmail.com>
2023-04-26 12:17:12 -0500

    +add | modules seeds by jobs

--- Database/Seeders/IeventDatabaseSeeder.php
+++ Database/Seeders/IeventDatabaseSeeder.php
@@ -4,19 +4,21 @@ namespace Modules\Ievent\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Isite\Jobs\ProcessSeeds;
 
 class IeventDatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        Model::unguard();
-
-        $this->call(IeventModuleTableSeeder::class);
-        // $this->call("OthersTableSeeder");
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    Model::unguard();
+    ProcessSeeds::dispatch([
+      "baseClass" => "\Modules\Ievent\Database\Seeders",
+      "seeds" => ["IeventModuleTableSeeder"]
+    ]);
+  }
 }
